2258550fdec82727f2767052b945d11d11bf3c15
EDGE <julien.fantino@gmail.com>
2015-02-09 14:56:05 +0100

    modif panier

--- MvcApplication3/MvcApplication3/Models/Paniers.cs
+++ MvcApplication3/MvcApplication3/Models/Paniers.cs
@@ -10,23 +10,23 @@ namespace MvcApplication3.Models
 
 
 
-        +CompteurArticle()
-+IsVerrouiller()
-+ViderPanier()
-+Montant(Int Quantite_Produit)
-+Montant_Global()
-+Supprimer_Article(String Nom_Produit)
-+ModifierQuantiterArticle(String Nom_Produit, Int Quantiter_Produit)
-+AjouterArticle(String Nom_Produit, Int Quantiter_Produit, Float Prix_Produit)
-+CreationPanier()
-+ValiderPanier()
-+AjoutCommande(Date Date_Commande
-Date Date_Echeance, Float Montant_Total, Float Montant_Secu, Float Montant_Mutuelle, Float Remboursement_Total, Bool Paiement, String Question, String Reponse, Int ID_Etat)
-+AfficherPanierSite()
-+ModifierPanier()
-+ProduitIndisponible(String Nom_Produit, Int Quantite_Produit)
+//        +CompteurArticle()
+//+IsVerrouiller()
+//+ViderPanier()
+//+Montant(Int Quantite_Produit)
+//+Montant_Global()
+//+Supprimer_Article(String Nom_Produit)
+//+ModifierQuantiterArticle(String Nom_Produit, Int Quantiter_Produit)
+//+AjouterArticle(String Nom_Produit, Int Quantiter_Produit, Float Prix_Produit)
+//+CreationPanier()
+//+ValiderPanier()
+//+AjoutCommande(Date Date_Commande
+//Date Date_Echeance, Float Montant_Total, Float Montant_Secu, Float Montant_Mutuelle, Float Remboursement_Total, Bool Paiement, String Question, String Reponse, Int ID_Etat)
+//+AfficherPanierSite()
+//+ModifierPanier()
+//+ProduitIndisponible(String Nom_Produit, Int Quantite_Produit)
 
-+DecrementerProduit()
+//+DecrementerProduit()
 
 
 
